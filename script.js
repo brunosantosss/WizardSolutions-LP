e53f6d55628e9a3d2ad8f311576e911d10cc8f83
brunosantosss <85379055+brunosantosss@users.noreply.github.com>
2025-04-09 21:41:58 -0300

    feature / adicionado mobile para smartphones

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
+    /*
+        Verificando a altura do scroll e setando classes de 
+        Estilização espeficica para a navbar.
+    */
     const navbar = document.getElementById("navbar");
     const stickyOffSet = navbar.offsetTop;
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > stickyOffSet) {
-        navbar.classList.add("nav-sticky");
+            navbar.classList.add("nav-sticky");
         } else {
-        navbar.classList.remove("nav-sticky");
+            navbar.classList.remove("nav-sticky");
         }
     });
 
+    // Botão Scroll Continuous da Hero Section.
     const nextSection = document.getElementById("next-section");
     nextSection.addEventListener('click', ()=> {
         nextSection.style.display = "none";
     });
 
+    /*
+        Verificações e funções para o sistema de multiplos idiomas.
+        - Português (Brasil)
+        - Inglês
+        - Espanhol
+    */
     const langsButton = document.getElementById("langs-button");
     const langsDropdown = document.getElementById("langs-dropdown");
     langsButton.addEventListener('click', () => {
@@ -64,4 +75,17 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const savedLang = localStorage.getItem('lang') || navigator.language.slice(0, 2);
     loadLanguage(savedLang);
+
+    // Navbar para mobiles
+    const menuMobile = document.getElementById("menu-mobile");
+    const menuMobileBTN = document.getElementById("menu-mobile-btn");
+
+    menuMobileBTN.addEventListener('click', () => {
+        if (menuMobile.classList.contains("hidden")) {
+            menuMobile.classList.remove("hidden");
+        }
+        else {
+            menuMobile.classList.add("hidden");
+        }
+    });
 });
